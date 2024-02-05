15cec7e9f24e42b6c47c3c0bcedaf7ddfc6d2ab1
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-05 20:19:22 +0530

    navbar config

--- firebase-contact-app/src/App.jsx
+++ firebase-contact-app/src/App.jsx
@@ -2,8 +2,14 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
+      <div className="flex">
+        <input
+          type="text"
+          className="border flex-grow h-10 bg-transparent border-white rounded-md"
+        />
+      </div>
     </div>
   );
 };
