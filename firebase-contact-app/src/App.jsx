05823399ca2a88f6d0329139e1653577f7c84402
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-05 20:51:29 +0530

    interface of ap

--- firebase-contact-app/src/App.jsx
+++ firebase-contact-app/src/App.jsx
@@ -1,14 +1,21 @@
-import Navbar from "./components/Navbar";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
 
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
-      <div className="flex">
-        <input
-          type="text"
-          className="border flex-grow h-10 bg-transparent border-white rounded-md"
-        />
+      <div className="flex gap-2">
+        <div className="relative flex items-center flex-grow">
+          <FiSearch className="absolute ml-1 text-white text-3xl" />
+          <input
+            type="text"
+            className="border flex-grow h-10 bg-transparent border-white pl-9 text-white rounded-md"
+          />
+        </div>
+
+        <AiFillPlusCircle className="text-5xl  cursor-pointer text-white" />
       </div>
     </div>
   );
--- firebase-contact-app/src/components/Navbar.jsx
+++ firebase-contact-app/src/components/Navbar.jsx
@@ -1,9 +1,6 @@
-import { FiSearch } from "react-icons/fi";
-
 const Navbar = () => {
   return (
     <div className="h-[60px] gap-2 bg-white my-4 rounded-lg text-xl flex justify-center items-center font-medium">
-      <FiSearch />
       <img src="/logos_firebase.svg" alt="firebase-logo" />
       <h1>Firebase Contact App</h1>
     </div>
