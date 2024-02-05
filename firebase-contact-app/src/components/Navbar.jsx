44eb776b7c52238640fe5a6704f118fc2b28de63
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-05 20:26:12 +0530

    readme init

--- firebase-contact-app/src/components/Navbar.jsx
+++ firebase-contact-app/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import { IoSearchOutline } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
   return (
     <div className="h-[60px] gap-2 bg-white my-4 rounded-lg text-xl flex justify-center items-center font-medium">
-      <IoSearchOutline />
+      <FiSearch />
       <img src="/logos_firebase.svg" alt="firebase-logo" />
       <h1>Firebase Contact App</h1>
     </div>
