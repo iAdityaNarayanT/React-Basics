f0d99ace2731df1119d67f6ee995634b12ca510d
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-06 01:03:26 +0530

    contact UI

--- firebase-contact-app/src/App.jsx
+++ firebase-contact-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 import { HiOutlineUserCircle } from "react-icons/hi";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
+
 import Navbar from "./components/Navbar";
 import { db } from "./config/firebase";
 const App = () => {
@@ -38,13 +41,22 @@ const App = () => {
 
         <AiFillPlusCircle className="text-5xl  cursor-pointer text-white" />
       </div>
-      <div>
+      <div className="mt-4">
         {contacts.map((contact) => (
-          <div key={contact.id}>
-            <HiOutlineUserCircle />
-            <div className="">
-              <h2 className="">{contact.name}</h2>
-              <p className="">{contact.email}</p>
+          <div
+            key={contact.id}
+            className="flex justify-around bg-yellow items-center rounded-lg p-2"
+          >
+            <div className="flex gap-1">
+              <HiOutlineUserCircle className="text-orange text-4xl" />
+              <div className="">
+                <h2 className="font-medium">{contact.name}</h2>
+                <p className="text-sm">{contact.email}</p>
+              </div>
+            </div>
+            <div className="flex text-3xl">
+              <RiEditCircleLine />
+              <IoMdTrash className="text-orange" />
             </div>
           </div>
         ))}
