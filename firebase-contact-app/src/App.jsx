37179e8360d8ba94416de0adc6aa16e6e88c584b
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-05 23:38:50 +0530

    useEffect Hook

--- firebase-contact-app/src/App.jsx
+++ firebase-contact-app/src/App.jsx
@@ -1,8 +1,20 @@
+import { collection, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
-
 import Navbar from "./components/Navbar";
+import { db, getDocs } from "./config/firebase";
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+  //use effect hook to perform network call
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+      } catch (error) {}
+    };
+  });
   return (
     <div className="mx-auto max-w-[370px] px-4">
       <Navbar />
