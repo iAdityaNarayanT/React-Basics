106dd6251143d4ff57e70ab1206b88cab48d2a3d
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-07 00:18:29 +0530

    import formik

--- firebase-contact-app/src/components/AddAndUpdateContact.jsx
+++ firebase-contact-app/src/components/AddAndUpdateContact.jsx
@@ -1,10 +1,31 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
 import Modal from "./Modal";
 
 const AddAndUpdateContact = ({ isOpen, onClose }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      Hi
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        <Form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1">
+            <label htmlFor="name">Name</label>
+            <Field name="name" className="h-10 border" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email</label>
+            <Field name="email" className="h-10 border" />
+          </div>
+          <button className="border bg-orange px-3 py-1.5 self-end">
+            Add contact
+          </button>
+        </Form>
+      </Formik>
     </Modal>
   );
 };
