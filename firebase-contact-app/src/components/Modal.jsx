2f26acb78d598d7deca079d79df9c1cb6fc348df
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-07 01:59:15 +0530

    onClose funciton removed'

--- firebase-contact-app/src/components/Modal.jsx
+++ firebase-contact-app/src/components/Modal.jsx
@@ -7,10 +7,7 @@ const Modal = ({ onClose, isOpen, children }) => {
   return createPortal(
     <div>
       {isOpen && (
-        <div
-          onClick={onClose}
-          className=" grid place-items-center absolute top-0 z-40 backdrop-blur h-screen w-screen"
-        >
+        <div className=" grid place-items-center absolute top-0 z-40 backdrop-blur h-screen w-screen">
           <div className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4">
             <div className="flex justify-end">
               <AiOutlineClose className="self-end text-2xl" onClick={onClose} />
