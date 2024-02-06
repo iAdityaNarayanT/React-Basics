2254524ca5cb1696dd07ffcb548546db23e9e2ef
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-07 01:52:38 +0530

    Form layout fixed

--- firebase-contact-app/src/components/Modal.jsx
+++ firebase-contact-app/src/components/Modal.jsx
@@ -7,17 +7,16 @@ const Modal = ({ onClose, isOpen, children }) => {
   return createPortal(
     <div>
       {isOpen && (
-        <div className=" absolute top-0 z-40 backdrop-blur h-screen w-screen">
-          <div className="relative z-50 m-auto min-h-[200px] max-w-[80%] bg-white p-4">
+        <div
+          onClick={onClose}
+          className=" grid place-items-center absolute top-0 z-40 backdrop-blur h-screen w-screen"
+        >
+          <div className="relative z-50 m-auto min-h-[200px] min-w-[80%] bg-white p-4">
             <div className="flex justify-end">
               <AiOutlineClose className="self-end text-2xl" onClick={onClose} />
             </div>
             {children}
           </div>
-          <div
-            onClick={onClose}
-            className=" absolute top-0 z-40 backdrop-blur h-screen w-screen"
-          />
         </div>
       )}
     </div>,
