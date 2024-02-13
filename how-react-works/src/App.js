635d45df27da75e2791e538bb902a43d2ace8ac4
Aditya Narayan Tiwari <vipraaditya@gmail.com>
2024-02-13 22:17:42 +0530

    activeTab

--- how-react-works/src/App.js
+++ how-react-works/src/App.js
@@ -36,6 +36,10 @@ function Tabbed({ content }) {
         <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
+        {/* {content.map((_, i) => (
+          <Tab key={i} num={i} activeTab={activeTab} onClick={setActiveTab} />
+        ))}
+         */}
       </div>
 
       {activeTab <= 2 ? (
